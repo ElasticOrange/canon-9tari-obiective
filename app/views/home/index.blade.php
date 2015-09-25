bcaa8c6e2f4b2c651ecae9e6d65a8e329c9b9bfb
AlinPMacovei <macoveialinp@gmail.com>
2015-09-25 14:17:05 +0300

    Home page translated

--- app/views/home/index.blade.php
+++ app/views/home/index.blade.php
@@ -4,10 +4,8 @@
     <div class="container-picture">
         <img src="img/pagina_intro.png"/>
         <div class="ugly-circle"></div>
-        <div class="text-intro">Aparatul foto reprezintă jumătate din povestea unei fotografii!</div>
-        <div class="text-intro-second">Cealaltă jumătate o reprezintă obiectivele. Descoperă ce obiectiv ţi se potriveşte şi intră
-automat în competiţie pentru a câştiga un obiectiv CANON</div>
-        <button type="button" class="button-descopera">Intră în aplicaţie aici!</button>
+        <div class="text-intro">Фотоапаратът е само половината от снимката!</div>
+        <button type="button" class="button-descopera">Влезте в приложението тук!</button>
     </div>
 
     <input type="submit" class="submit-button" value="Submit"/>
