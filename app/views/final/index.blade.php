beb07fcbc1258399248f4df9a8807c7a6e3df570
Mihai Barbu <xwave21@gmail.com>
2015-10-02 12:03:14 +0300

    Remove regulament. Add last page and modify routes, game js. Debug false.

--- app/views/final/index.blade.php
+++ app/views/final/index.blade.php
@@ -2,19 +2,8 @@
 
 @section('content')
     <div class="container-picture">
-        <div class="ugly-circle"></div>
-        <img src="img/pagina_intro.png"/>
-        <div class="text-intro text-intro-final">Multumim pentru participare!</div>
-        <div class="text-intro-second"></div>
+        <img src="img/carton_lab_lens2.jpg">
     </div>
-
-    <!--Obiective-->
-    <input type="submit" class="submit-button" value="Submit"/>
-    <div class="obiectiv_unu obiective obiective-submit"><img src="img/Obiective/obiectiv2.png"/></div>
-    <div class="obiectiv_doi obiective obiective-submit"><img src="img/Obiective/obiectiv1.png"/></div>
-    <div class="obiectiv_trei obiective obiective-submit"><img src="img/Obiective/obiectiv4.png"/></div>
-    <div class="obiectiv_patru obiective obiective-submit"><img src="img/Obiective/obiectiv3.png"/></div>
-    <!--Obiective-->
 @stop
 
 @section('js')
