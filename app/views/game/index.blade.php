4e3aba52579c718e5cdc77f179516ea4e51c616f
Vlad <vladrotaru@elasticorange.com>
2015-10-15 15:25:54 +0300

    Change photo and text for 50mm lens

--- app/views/game/index.blade.php
+++ app/views/game/index.blade.php
@@ -56,7 +56,7 @@
 
     <img class="obiectiv-kit" id="obiectiv-kit" src="/img/Obiective/obiectiv2.png">
     <img class="obiectiv-kit" id="obiectiv-wide" src="/img/Obiective/obiectiv1.png">
-    <img class="obiectiv-kit" id="obiectiv-portrait" src="/img/Obiective/obiectiv4.png">
+    <img class="obiectiv-kit" id="obiectiv-portrait" src="/img/Obiective/canon_50mm.png">
     <img class="obiectiv-kit" id="obiectiv-tele" src="/img/Obiective/obiectiv3.png">
     <div class="text-container">
     </div>
@@ -75,7 +75,7 @@
 
     <script type="text/template" id="display_lens_3">
         <div class="preview-lens1 preview-lens3"></div>
-        <div class="preview-title">Canon<br /> EF 50mm f/1.8 II </div>
+        <div class="preview-title">Canon<br /> EF 50mm f/1.8 STM </div>
         <div class="preview-content">Ο φακός EF 50mm είναι ιδανικός για πορτραίτα, καθώς θολώνει το φόντο και εστιάζει στο σημαντικότερο στοιχείο της λήψης: το θέμα.</div>
     </script>
 
@@ -115,7 +115,7 @@
 
     <script type="text/template" id="texts-portrait">
         <span class="text-container-title">
-            Canon EF 50mm f/1.8 II
+            Canon EF 50mm f/1.8 STM
         </span><br />
         <p class="text-container-description">
             Με τον φακό EF 50mm f/1.8 STM μπορείτε να δημιουργήσετε το ανόθευτο οπτικό συναίσθημα που χρειάζεται για κάθε 
